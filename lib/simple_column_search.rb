82f8b6cf4dceef48e6e6aa78e8c6dde8a2690e78
肖盛允 <Shengyun.xiao@taodinet.com>
2014-01-10 14:58:28 +0800

    Retention ActiveRecord 3

--- lib/simple_column_search.rb
+++ lib/simple_column_search.rb
@@ -29,7 +29,7 @@ module SimpleColumnSearch
 
     # PostgreSQL LIKE is case-sensitive, use ILIKE for case-insensitive
     like = connection.adapter_name == "PostgreSQL" ? "ILIKE" : "LIKE"
-    # Determine if ActiveRecord 3 And ActiveRecord 4
+    # Retention ActiveRecord 3
     scope options[:name], lambda { |terms|
       terms = options[:escape].call(terms) if options[:escape]
       conditions = terms.split.inject(where(nil)) do |acc, term|
